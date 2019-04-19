a08cf226d160fe2eba34c3ffda6a00da0f48bcbc
Rubén Castro <racingsoft@hotmail.es>
2019-04-20 00:10:29 +0200

    BrakeSensor

--- brake/BrakeSensor.cpp
+++ brake/BrakeSensor.cpp
@@ -9,30 +9,29 @@ HX711 presionSensor;
 void BrakeSensorClass::init()
 {
 	presionSensor.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+	currentValue = LOADCELL_MINVALUE;
 	calibrationDone = false;	
 }
 
 void BrakeSensorClass::doCalibration()
 {
-	// Check sensor
-	if (!presionSensor.wait_ready_timeout(1000))
+	// Calibration
+	if (presionSensor.is_ready())
+	{
+		currentValue = presionSensor.read_average(10);
+		Logger.info("Value: " + String(currentValue));
+
+		minValue = LOADCELL_MINVALUE;
+		maxValue = LOADCELL_MAXVALUE;
+		Logger.info("Min value: " + String(minValue));
+		Logger.info("Max value: " + String(maxValue));
+
+		calibrationDone = true;
+	}
+	else
 	{
 		Logger.error("Brake sensor not found");
-		return;
 	}
-
-	// Calibration
-
-	long value = presionSensor.read_average(10);
-	Logger.info("Value: " + String(value));
-
-	minValue = LOADCELL_MINVALUE;
-	maxValue = LOADCELL_MAXVALUE;
-	Logger.info("Min value: " + String(minValue));
-	Logger.info("Max value: " + String(maxValue));
-	
-
-	calibrationDone = true;
 }
 
 bool BrakeSensorClass::isCalibrated()
@@ -42,24 +41,20 @@ bool BrakeSensorClass::isCalibrated()
 
 uint16_t BrakeSensorClass::read()
 {
-	// Check calibration
-	if (!calibrationDone)
-	{
-		Logger.error("Brake sensor not calibrated");
-		return 0;
-	}
-
 	// Reading uint16_t value
-	currentValue = presionSensor.read();
-		
-	// Mapping value to 0 254 range
-	currentMappedValue = (uint16_t)map(currentValue, minValue, maxValue, 0, 254);
+	if (presionSensor.is_ready())
+	{
+		currentValue = presionSensor.read();
 
-	if (currentMappedValue < 0)
-		currentMappedValue = 0;
+		// Mapping value to 0 254 range
+		currentMappedValue = (uint16_t)map(currentValue, minValue, maxValue, 0, 254);
 
-	if (currentMappedValue > 254)
-		currentMappedValue = 254;
+		if (currentMappedValue < 0)
+			currentMappedValue = 0;
+
+		if (currentMappedValue > 254)
+			currentMappedValue = 254;
+	}
 
 	return currentMappedValue;
 }
